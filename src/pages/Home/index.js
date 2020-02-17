3f73493e78698585295629f38951397edae37b4d
afmelo92 <andre.fabian.melo@gmail.com>
2020-02-17 17:57:08 -0400

    Improved FlatList with styles

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,79 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import { Button, View, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import { FlatList } from 'react-native';
 
-export default function Home({ navigation }) {
+import {
+  Wrapper,
+  Container,
+  ProductContainer,
+  ProductImage,
+  ProductDescription,
+  ProductPrice,
+  ProductAmount,
+  ProductCounter,
+  ProductText,
+  ProductButton,
+} from './styles';
+
+export default function Home() {
+  const DATA = [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'Tênis Maneiro de Corrida',
+      price: 'R$179,90',
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: 'Tênis Maneiro de Esporte',
+      price: 'R$199,90',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145884842',
+      title: 'Tênis Maneiro de Futebol com alças',
+      price: 'R$99,90',
+    },
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3a844428ba',
+      title: 'Tênis Maneiro de Corrida',
+      price: 'R$179,90',
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbasda55a97f63',
+      title: 'Tênis Maneiro de Esporte',
+      price: 'R$199,90',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Tênis Maneiro de Futebol com alças',
+      price: 'R$99,90',
+    },
+  ];
+
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: '#191920',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      <Text style={{ color: 'white' }}>Hello World WEB ONLINE!</Text>
-      <Button title="To Cart" onPress={() => navigation.navigate('Cart')} />
-    </View>
+    <Wrapper>
+      <Container>
+        <FlatList
+          horizontal
+          data={DATA}
+          renderItem={({ item }) => (
+            <ProductContainer key={item.id}>
+              <ProductImage />
+              <ProductDescription>{item.title}</ProductDescription>
+              <ProductPrice>{item.price}</ProductPrice>
+
+              <ProductButton>
+                <ProductAmount>
+                  <Icon name="add-shopping-cart" size={15} color="#FFF" />
+                  <ProductCounter>3</ProductCounter>
+                </ProductAmount>
+                <ProductText>Adicionar ao carrinho</ProductText>
+              </ProductButton>
+            </ProductContainer>
+          )}
+        />
+      </Container>
+    </Wrapper>
   );
 }
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,65 @@
+import { darken } from 'polished';
+import styled from 'styled-components/native';
+
+export const Wrapper = styled.SafeAreaView`
+  flex: 1;
+  background: #191920;
+`;
+
+export const Container = styled.View``;
+
+export const ProductContainer = styled.View`
+  width: 220px;
+  background: #fff;
+  border-radius: 4px;
+  margin: 5px;
+  padding: 5px;
+  align-items: center;
+`;
+
+export const ProductImage = styled.View`
+  background: red;
+  width: 200px;
+  height: 200px;
+  border-radius: 10px;
+`;
+
+export const ProductDescription = styled.Text.attrs({
+  numberOfLines: 2,
+})`
+  color: #777;
+`;
+
+export const ProductPrice = styled.Text`
+  width: 100%;
+  margin-bottom: 10px;
+`;
+
+export const ProductButton = styled.TouchableOpacity`
+  background: #7159c1;
+  flex-direction: row;
+  align-items: center;
+  border-radius: 4px;
+  margin-top: auto;
+`;
+
+export const ProductAmount = styled.View`
+  padding: 10px;
+  background: ${darken(0.1, '#7159c1')};
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const ProductCounter = styled.Text`
+  color: #fff;
+  margin-left: 5px;
+`;
+
+export const ProductText = styled.Text`
+  flex: 1;
+  text-align: center;
+  font-weight: bold;
+  color: #fff;
+`;
